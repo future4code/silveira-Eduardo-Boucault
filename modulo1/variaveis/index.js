fc08e8c4b0983b3ecdb593721e98ce33578e9427
Eduardo Boucault <du.boucault@gmail.com>
2022-02-15 19:10:56 -0300

    variaveis

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -66,6 +66,3 @@ const ex2 = 30;
 console.log ("O primeiro número somado ao segundo número resulta em:" , ex1 + ex2);
 
 console.log ("O primeiro número multiplicado pelo segundo número resulta em:" , ex1 * ex2);
-
-
-
