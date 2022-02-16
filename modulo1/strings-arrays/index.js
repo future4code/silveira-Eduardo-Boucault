366db05360b958f042150a12a83f9149c54855ab
Eduardo Boucault <du.boucault@gmail.com>
2022-02-16 16:54:44 -0300

    strings-arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,94 @@
+//Exercícios de interpretação de códigos:
+//Ex 1.
+//a) No console impresso mostrará uma lista sem elementos portando não definida.
+//b) No console impresso mostrará uma lista null.
+//c) No console impresso mostrará uma lista com length de 11 elementos.
+//d)
+//e) 
+//f)
+
+// let array
+// console.log('a. ', array)
+
+// array = null
+// console.log('b. ', array)
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length)
+
+// let i = 0
+// console.log('d. ', array[i])
+
+// array[i+1] = 19
+// console.log('e. ', array)
+
+// const valor = array[i+6]
+// console.log('f. ', valor)
+
+//Ex 2.
+
+//a)
+// "Subi num ônibus em Marrocos"
+// "SUBI NUM ÔNIBUS EM MIRROCOS"
+//LENGTH DE 27 CARACTERES
+
+//Exercícios de escrita de códigos:
+//Ex 1.
+
+//a)
+
+// const nomeDoUsuario = prompt('Qual é seu nome?')
+// const emailDoUsuario = prompt('Qual é seu email?')
+// const dados = `O email ${emailDoUsuario} foi cadastrado com sucesso. Seja bem vinda(o), ${nomeDoUsuario}!`
+
+// console.log(dados)
+
+//Ex 2.
+
+// const listaDeComidas =['Pizza', 'Pastel', 'Esfihas', 'Lasanha', 'Churrasco'];
+//Index                  0        1          2         3           4
+
+//a)
+
+// console.log(listaDeComidas);
+
+//b)
+
+// let mensagemComida = ('Essas são minhas comidas preferidas:');
+
+// console.log(mensagemComida);
+
+// console.log(listaDeComidas[0]);
+// console.log(listaDeComidas[1]);
+// console.log(listaDeComidas[2]);
+// console.log(listaDeComidas[3]);
+// console.log(listaDeComidas[4]);
+
+//c)
+
+// let novaComida = prompt('Qual sua comida favorita?')
+
+// console.log(listaDeComidas[0]);
+// console.log(listaDeComidas[1] = novaComida);
+// console.log(listaDeComidas[2]);
+// console.log(listaDeComidas[3]);
+// console.log(listaDeComidas[4]);
+
+
+//Ex 3.
+//a)
+
+// let listaDeTarefas = ['Tarefa 1' , 'Tarefa 2', 'Tarefa 3'];
+//Index                   0             1          2
+
+// let tarefa1 = prompt('Você já limpou seu quarto?')
+// let tarefa2 = prompt('Você já tomou banho?')
+// let tarefa3 = prompt('Você já deu ração para o cachorro?')
+
+// console.log(listaDeTarefas[0] = tarefa1)
+// console.log(listaDeTarefas[1] = tarefa2)
+// console.log(listaDeTarefas[2] = tarefa3)
+
+// let tarefaConcluida = promtp('Qual tarefa dessas você já realizou?')
+
+//Não consegui completar mas continuarei tentando.
